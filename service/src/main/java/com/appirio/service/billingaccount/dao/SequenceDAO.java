d074ae9b2f6c6e51a42eed15b918c5df2afc02b9
Guiqiang Zhang <skyhit@gmail.com>
2018-03-28 12:06:42 +0800

    fix database name

--- service/src/main/java/com/appirio/service/billingaccount/dao/SequenceDAO.java
+++ service/src/main/java/com/appirio/service/billingaccount/dao/SequenceDAO.java
@@ -6,7 +6,7 @@ import com.appirio.supply.dataaccess.DatasourceName;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.customizers.Define;
 
-@DatasourceName("common")
+@DatasourceName("oltp")
 public interface SequenceDAO {
     @SqlQuery("SELECT SEQUENCE_<sequenceName>.nextval AS nextId FROM systables WHERE tabid = 1")
     IdSequence getIdSequence(@Define("sequenceName") String sequenceName);
