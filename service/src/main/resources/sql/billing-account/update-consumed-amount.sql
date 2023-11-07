d1b5ce95c0792186694ab8d57964d7d71b36f615
Rakib Ansary <ansaryfantastic@gmail.com>
2023-11-07 12:39:52 +0000

    Revert "Billing Account LockedAmount and ConsumedAmount update"

--- service/src/main/resources/sql/billing-account/update-consumed-amount.sql
+++
@@ -1,6 +0,0 @@
-UPDATE project
-SET
-    consumed_amount = NVL(consumed_amount, 0) + :requested_amount,
-    locked_amount = NVL(locked_amount, 0) - :unlock_amount
-WHERE
-    project_id = :project_id;
