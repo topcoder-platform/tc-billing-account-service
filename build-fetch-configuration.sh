cbf36d55bd26774acd041b87d389eea419019d68
Justin Gasper <jmgasper@gmail.com>
2024-09-13 07:52:08 +1000

    Fix prod build

--- build-fetch-configuration.sh
+++ build-fetch-configuration.sh
@@ -2,4 +2,10 @@
 set -e
 ENV=$1
 CONFIG=`echo "$ENV" | tr '[:upper:]' '[:lower:]'`
-aws s3 cp s3://appirio-platform-$CONFIG/application/tc-api-core/$CONFIG/TC.prod.ldap.newv2.keystore TC.prod.ldap.keystore
+
+if [ "$CONFIG" = "prod" ]; then
+    aws s3 cp s3://appirio-platform-$CONFIG/application/tc-api-core/$CONFIG/TC.prod.ldap.sept2024.keystore TC.prod.ldap.keystore
+fi
+if [ "$CONFIG" = "dev" ]; then
+    aws s3 cp s3://appirio-platform-$CONFIG/application/tc-api-core/$CONFIG/TC.prod.ldap.newv2.keystore TC.prod.ldap.keystore
+fi
