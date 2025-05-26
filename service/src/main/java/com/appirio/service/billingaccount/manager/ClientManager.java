deb7584b89d542de9c7f13d798c65a1f6f56d65f
Justin Gasper <jmgasper@gmail.com>
2025-05-27 08:56:38 +1000

    Logging

--- service/src/main/java/com/appirio/service/billingaccount/manager/ClientManager.java
+++ service/src/main/java/com/appirio/service/billingaccount/manager/ClientManager.java
@@ -123,6 +123,7 @@ public class ClientManager extends BaseManager {
         } else {
             // validate if the provided client name is unique in the persistence.
             Client clientWithInputName = clientDAO.getClientByName(clientDTO.getName());
+            System.out.println("ClientDTO name: " + clientDTO.getName() + "clientWithInputNameID: " + clientWithInputName.getId() + "existingClient.getId: " +  existingClient.getId());
             if (clientWithInputName != null && clientWithInputName.getId() != existingClient.getId()) {
                 // There is another client different than the client to update which have the new specified name
                 throw new IllegalArgumentException(String.format("The client name '%s' already exists",
